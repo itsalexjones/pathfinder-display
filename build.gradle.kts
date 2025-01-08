6e165e25e945556b4688025f00c2373155c6156d
Alex Jones <alex.jones@global.com>
2025-01-08 17:26:56 +0000

    Revert "Add license"
    This reverts commit f73a0582a0291e8668706d162b8ea99e5cd503ac.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,3 @@
-/*
- * "THE BEER-WARE LICENSE"
- * Alex Jones (alex [at] alexjones [dot] radio) wrote this file.
- * As long as you retain this notice you can do whatever you want with this stuff.
- * If we meet some day, and you think this stuff is worth it, you can buy me a beer in return.
- */
-
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
